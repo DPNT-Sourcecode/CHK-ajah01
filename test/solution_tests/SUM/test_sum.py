bcb85a0988877adb543d67cb6097310a9bac852f
sbx_user1051 <sbx_user1051@169.254.198.25>
2022-06-10 17:06:35 +0000

    Fri Jun 10 17:06:35 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,5 +3,5 @@ import unittest
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 5
-
+        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(3, 2) == 5
